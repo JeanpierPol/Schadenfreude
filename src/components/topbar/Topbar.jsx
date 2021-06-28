e6ee5f9d67e63e1909a6f4e5b7d1c6ec00fbde85
Jeanpier <je5x.1.jnes@gmail.com>
2021-06-28 11:22:16 +0200

    asdasd

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,45 +1,46 @@
-import React from 'react'
-import { Search, Person, Chat, Notifications } from '@material-ui/icons'
+import React from "react";
+import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
 export default function Topbar() {
-    return (
-        <div>
-            <div className="topbarContainer">
-                <div className="topbarLeft">
-                    <span className="logo">schadenfreude</span>
-                </div>
-                <div className="topbarCenter">
-                    <div className="searchbar">
-                        <Search className="searchIcon"/>
-                        <input placeholder="Search" type="text" className="seachInput" />
-                    </div>
-                </div>
-                <div className="topbarRight">
-                    <div className="topbarLinks">
-                        <span className="topbarLinks">Home</span>
-                        <span className="topbarLinks">Home</span>
-
-                    </div>
-
-                    <div className="topbarIcons">
-                        <div className="topbarIconItem">
-                            <Person />
-                            <span className="topbarIconBadge">1</span>
-                        </div>
-                        <div className="topbarIconItem">
-                            <Chat />
-                            <span className="topbarIconBadge">1</span>
-                        </div>
-                        <div className="topbarIconItem">
-                            <Notifications />
-                            <span className="topbarIconBadge">1</span>
-                        </div>
-                    </div>
-
-                    <img src="\assets\img\zavatar.png" alt="" className="topbarImg" />
-                </div>
-
-            </div>
+  return (
+    <div>
+      <div className="topbarContainer">
+        <div className="topbarLeft">
+          <Link to="/" style={{textDecoration:"none"}}>
+            <span className="logo">schadenfreude</span>
+          </Link>
         </div>
-    )
+        <div className="topbarCenter">
+          <div className="searchbar">
+            <Search className="searchIcon" />
+            <input placeholder="Search" type="text" className="seachInput" />
+          </div>
+        </div>
+        <div className="topbarRight">
+          <div className="topbarLinks">
+            <span className="topbarLinks">Home</span>
+            <span className="topbarLinks">Home</span>
+          </div>
+
+          <div className="topbarIcons">
+            <div className="topbarIconItem">
+              <Person />
+              <span className="topbarIconBadge">1</span>
+            </div>
+            <div className="topbarIconItem">
+              <Chat />
+              <span className="topbarIconBadge">1</span>
+            </div>
+            <div className="topbarIconItem">
+              <Notifications />
+              <span className="topbarIconBadge">1</span>
+            </div>
+          </div>
+
+          <img src="\assets\img\zavatar.png" alt="" className="topbarImg" />
+        </div>
+      </div>
+    </div>
+  );
 }
